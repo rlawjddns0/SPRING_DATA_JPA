2b1987a1fd912476e4229b60143f397983fc8ea7
KJW <rlawjddns0@naver.com>
2023-02-19 22:29:16 +0900

    Team Repo init
    Team Repo init

--- data-jpa/src/main/java/study/datajpa/repository/TeamRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/TeamRepository.java
@@ -3,11 +3,25 @@ package study.datajpa.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
+import study.datajpa.entity.Team;
+
+import java.util.List;
 
 @Repository
 public class TeamRepository {
     @PersistenceContext
     private EntityManager em;
 
+
+    public Team findById(Long id){
+        return em.find(Team.class,id);
+
+
+    }
+
+    public List<Team> findAll(){
+        return em.createQuery("select t from Team t",Team.class)
+                .getResultList();
+    }
 
 }
